a354aea0758df481f84d3455eaa650e3f4e7fa95
cmDora <18311256237@163.com>
2018-12-15 13:32:04 +0800

    cm_checkout

--- community-webapp/build/webpack.config.server.js
+++ community-webapp/build/webpack.config.server.js
@@ -1,6 +1,8 @@
 const path = require('path')
+const webpackMerge = require('webpack-merge')
+const baseConfig = require('./webpack.base')
 
-module.exports = {
+module.exports = webpackMerge(baseConfig, {
   mode: 'development',
   target: 'node',
   entry: {  // 打包的入口
@@ -8,27 +10,6 @@ module.exports = {
   },
   output: { // 打包的出口
     filename: 'server-entry.js',
-    path: path.join(__dirname, '../dist'),
-    publicPath: '/public',
     libraryTarget: 'commonjs2' // 打包出来的 js 使用的一种模块的方案。如 umd、cmd(cjs)、amd(requirejs)、commonjs、global
-  },
-  module: {
-    rules: [
-      {
-        enforce: 'pre',
-        test: /\.(js|jsx)$/,
-        loader: 'eslint-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /.jsx$/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      }
-    ]
   }
-}
+})
